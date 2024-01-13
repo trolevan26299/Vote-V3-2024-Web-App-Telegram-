1d3985da2b34a12929983b4c41301d4453391d93
andrew <trolevan26299@gmail.com>
2024-01-13 15:46:44 +0700

    update code

--- src/config-global.ts
+++ src/config-global.ts
@@ -5,7 +5,7 @@ import { paths } from 'src/routes/paths';
 // ----------------------------------------------------------------------
 
 // export const HOST_API = process.env.NEXT_PUBLIC_HOST_API;
-export const HOST_API = 'https://vote-v3.vercel.app';
+export const HOST_API = 'https://api-dev-minimal-v510.vercel.app';
 export const ASSETS_API = process.env.NEXT_PUBLIC_ASSETS_API;
 
 // ROOT PATH AFTER LOGIN SUCCESSFUL
