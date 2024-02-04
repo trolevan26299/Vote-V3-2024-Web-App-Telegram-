7aea7a481cefaffac28af563aeb24348f8cb788c
andrew <trolevan26299@gmail.com>
2024-02-04 09:42:59 +0700

    update code

--- src/sections/shareholder_process/view/process-dh-view.tsx
+++ src/sections/shareholder_process/view/process-dh-view.tsx
@@ -90,6 +90,7 @@ export default function ProcessDHView() {
     // Nếu không thỏa mãn điều kiện hoặc không có phần tử nào thỏa mãn trong gui_den, trả về false
     return false;
   });
+  console.log('-------------------------', filteredData);
 
   const pollDataByKey = danhSachPollData.find((poll) => poll.key === questionSelect);
 
