abc2ad6a8434cefb57c0204ccbd7c4b176d21ddd
andrew <trolevan26299@gmail.com>
2024-02-18 13:26:03 +0700

    update code

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -7,13 +7,13 @@ import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDqpMZ7z9JjPcgT2jvxR3zi-jrF9Jz7x84',
-  authDomain: 'fir-project-2a706.firebaseapp.com',
-  databaseURL: 'https://fir-project-2a706-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'fir-project-2a706',
-  storageBucket: 'fir-project-2a706.appspot.com',
-  messagingSenderId: '363286697426',
-  appId: '1:363286697426:web:e6faaf731d86acec5aa40b',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
 // Initialize Firebase
