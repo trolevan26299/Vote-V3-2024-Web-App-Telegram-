60358f1b67e045adf7a44f71080cd44ba521893d
andrew <trolevan26299@gmail.com>
2024-02-21 09:32:53 +0700

    update 21/2

--- src/sections/vote-dh/view/vote-dh-view.tsx
+++ src/sections/vote-dh/view/vote-dh-view.tsx
@@ -213,10 +213,7 @@ export default function VoteDHView() {
         ? listHistoryVoted.find((item) => item?.ma_cd === user?.ma_cd)
         : undefined; // Tìm xem đã gửi voted lần nào chưa
     console.log('dataExist', dataExist);
-    const historyVotedRef = ref(
-      database,
-      `poll_process/ls_poll/${dataExist ? dataExist?.key : ''}`
-    );
+    const historyVotedRef = ref(database, `poll_process/ls_poll`);
     console.log('historyVotedRef', historyVotedRef);
     try {
       console.log('có chạy vào try');
