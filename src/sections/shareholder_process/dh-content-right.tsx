4af8f05123293ce0ab73a64b2865a94af477a5f4
andrew <trolevan26299@gmail.com>
2024-02-22 17:39:02 +0700

    update code

--- src/sections/shareholder_process/dh-content-right.tsx
+++ src/sections/shareholder_process/dh-content-right.tsx
@@ -184,7 +184,7 @@ export default function DHContentRight({
       show: false,
     },
     xaxis: {
-      categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+      categories: ['Câu 1', 'Câu 2', 'Câu 3', 'Câu 4'],
     },
 
     ...options,
