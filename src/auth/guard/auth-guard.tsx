419d78a3c591aea27ace39d04ce1a1c0609a2612
andrew <trolevan26299@gmail.com>
2024-02-25 17:22:20 +0700

    update code

--- src/auth/guard/auth-guard.tsx
+++ src/auth/guard/auth-guard.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef } from 'react';
 import { PATH_AFTER_LOGIN } from 'src/config-global';
+import { useUser } from 'src/firebase/user_accesss_provider';
 import { paths } from 'src/routes/paths';
 import { useTelegram } from 'src/telegram/telegram.provider';
-import { useRouter } from '../../routes/hooks/index';
+import { usePathname, useRouter } from '../../routes/hooks/index';
 
 type Props = {
   children: React.ReactNode;
@@ -11,6 +12,8 @@ type Props = {
 export default function AuthGuard({ children }: Props) {
   const router = useRouter();
   const userAccess = useTelegram();
+  const user = useUser();
+  const pathname = usePathname();
 
   const checked = useRef<boolean>(false);
 
@@ -22,12 +25,20 @@ export default function AuthGuard({ children }: Props) {
     if (userAccess?.user?.id) {
       localStorage.setItem('checked', JSON.stringify(true));
     } else if (checked.current === true) {
-      router.push(PATH_AFTER_LOGIN);
+      if (user) {
+        if (pathname === '/dashboard/question-and-answer') {
+          router.push(paths.dashboard.questionAndAnswerPath);
+        } else {
+          router.push(paths.dashboard.voteDH);
+        }
+      } else {
+        router.push(PATH_AFTER_LOGIN);
+      }
     } else {
       router.replace(paths.auth.jwt.login);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [checked]);
+  }, [checked, user]);
 
   if (!checked) {
     return null;
