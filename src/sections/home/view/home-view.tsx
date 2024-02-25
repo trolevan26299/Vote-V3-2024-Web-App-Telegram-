fb457663e2ea2793d969e3aa4f815c443aa6c41e
andrew <trolevan26299@gmail.com>
2024-02-25 15:05:39 +0700

    update code

--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useUser } from 'src/firebase/user_accesss_provider';
 import { useRouter } from 'src/routes/hooks';
 import { paths } from 'src/routes/paths';
@@ -10,17 +10,23 @@ export default function HomeView() {
   const router = useRouter();
   const telegramContext = useTelegram();
   const { user } = useUser();
+  const [userAccess, setUserAccess] = useState<number | undefined>(undefined);
 
-  useEffect(() => {
-    if (telegramContext?.user?.id) {
+  const checkRole = () => {
+    if (userAccess) {
       if (user) {
         router.push(paths.dashboard.voteDH);
       } else {
         router.push(paths.dashboard.permission);
       }
     } else {
-      router.push(paths.dashboard.settingVote.vote);
+      router.push(paths.dashboard.settingVote.info);
     }
+  };
+
+  useEffect(() => {
+    setUserAccess(telegramContext?.user?.id);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [telegramContext]);
   // eslint-disable-next-line react/jsx-no-useless-fragment
