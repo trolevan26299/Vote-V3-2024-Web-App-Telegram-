56c48ef6fcef9262175e65a754c479e4eeeddea6
levantro26299 <trolevan26299@gmail.com>
2024-02-27 02:54:00 +0700

    update code

--- src/auth/guard/auth-guard.tsx
+++ src/auth/guard/auth-guard.tsx
@@ -21,8 +21,13 @@ export default function AuthGuard({ children }: Props) {
     const storedChecked = localStorage.getItem('checked');
 
     checked.current = storedChecked ? JSON.parse(storedChecked) : false;
-    if (userAccess && user.user) {
+    if (userAccess && user.user && !checked.current) {
       localStorage.setItem('checked', JSON.stringify(true));
+      if (pathname === '/dashboard/question-and-answer') {
+        router.push(paths.dashboard.questionAndAnswerPath);
+      } else if (pathname === '/dashboard/vote-dh') {
+        router.push(paths.dashboard.voteDH);
+      }
     } else if (checked.current === true) {
       if (pathname === '/dashboard/question-and-answer') {
         router.push(paths.dashboard.questionAndAnswerPath);
@@ -30,7 +35,6 @@ export default function AuthGuard({ children }: Props) {
         router.push(paths.dashboard.voteDH);
       } else if (user.user !== undefined) {
         router.push(paths.dashboard.voteDH);
-        // router.push(paths.dashboard.questionAndAnswerPath);
       }
     } else {
       router.replace(paths.auth.jwt.login);
