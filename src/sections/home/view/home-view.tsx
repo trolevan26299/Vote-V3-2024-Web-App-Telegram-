d1a47b00fdad7df312d942a6442f7ce83fce575b
andrew <trolevan26299@gmail.com>
2024-02-26 13:51:07 +0700

    26/2 fix show popup when have a new poll

--- src/sections/home/view/home-view.tsx
+++ src/sections/home/view/home-view.tsx
@@ -1,27 +1,39 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useUser } from 'src/firebase/user_accesss_provider';
 import { usePathname, useRouter } from 'src/routes/hooks';
 import { paths } from 'src/routes/paths';
+import { useTelegram } from 'src/telegram/telegram.provider';
 
 export default function HomeView() {
   const router = useRouter();
   const { user } = useUser();
-  console.log('user:', user);
+  const [userAccess, setUserAccess] = useState<any>();
   const pathname = usePathname();
+  const telegramContext = useTelegram();
+
   useEffect(() => {
-    if (user) {
-      if (pathname === '/dashboard/question-and-answer') {
-        router.push(paths.dashboard.questionAndAnswerPath);
+    if (userAccess) {
+      if (user) {
+        if (pathname === '/dashboard/question-and-answer') {
+          router.push(paths.dashboard.questionAndAnswerPath);
+        } else {
+          router.push(paths.dashboard.voteDH);
+        }
       } else {
-        router.push(paths.dashboard.voteDH);
+        router.push(paths.page403);
       }
     } else {
       router.push(paths.dashboard.settingVote.vote);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    setUserAccess(telegramContext?.user);
+  }, [telegramContext]);
+
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <></>;
 }
